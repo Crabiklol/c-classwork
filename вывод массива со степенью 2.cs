57142958fcdad1c2eaffff0942190c9f93493c26
Crabiklol <kirakosyan1320@bk.ru>
2025-11-12 14:05:25 +0300

    Create вывод массива со степенью 2.cs

---
+++ вывод массива со степенью 2.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //const int size = 10;
+            //int[] arr = new int[size];
+            //Random rnd = new Random();
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(1, 10);
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine($"{arr[i]}");
+            //}
+
+            const int N = 10;
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = (int)Math.Pow(2, i);
+            }
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
